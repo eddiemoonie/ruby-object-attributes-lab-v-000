5701ac1e479c6358bf3352ad47cbbaf17b0dadac
Eddie Moon <eddiemoonie@gmail.com>
2018-01-25 22:30:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
 class Dog
   def name=(name)
-    @name = dogs_name
+    @name = name
   end
 
   def name
     @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
   end
 end
